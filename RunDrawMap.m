d917d647acfa998b318b7c93e3d4c43bde2cfd7b
ljs <ljs050@outlook.com>
2022-09-23 17:54:21 +0800

    Update RunDrawMap.m
    saving map as jpg

--- RunDrawMap.m
+++ RunDrawMap.m
@@ -7,9 +7,12 @@ init_csv;
 vedioWidth = 800;
 vedioHeight = 1170;
 %% load data and draw map
-load("/home/muscle/pacman/Code/vedioCode/data/Patamon-01-Jul-2021-1/1-1-Patamon-01-Jul-2021.mat","data");
+load("data/2-1-ljs-23-Sep-2022.mat","data");
 map = data.gameMap.currentTiles(:,1);
+
 init_gameVar;
 drawMap;
 Screen('Flip', gameWindow, 0, 0, 1);
+imageArray = Screen('GetImage', gameWindow);
+imwrite(imageArray, sprintf('Map-%s.jpg',datetime('now','Format','yMMdd-HHmm')))
 rmpath(genpath("src"))
